cc91e29cf3720836b98103e95fca47f19144be8e
Sayat <serat5915@gmail.com>
2023-06-16 18:14:02 +0600

    2023-06-16 18:13 Added Mail Functionality, Status Code needs fixing

--- src/main/java/dev/seratt/mailing_system_main/controller/SpamRestController.java
+++ src/main/java/dev/seratt/mailing_system_main/controller/SpamRestController.java
@@ -1,8 +1,11 @@
 package dev.seratt.mailing_system_main.controller;
 
+import dev.seratt.mailing_system_main.entity.EmailDetails;
 import dev.seratt.mailing_system_main.entity.Group;
 import dev.seratt.mailing_system_main.entity.Spam;
 import dev.seratt.mailing_system_main.entity.User;
+import dev.seratt.mailing_system_main.service.EmailService;
+import dev.seratt.mailing_system_main.service.EmailServiceImpl;
 import dev.seratt.mailing_system_main.service.GroupService;
 import dev.seratt.mailing_system_main.service.SpamService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,8 @@ public class SpamRestController {
     SpamService spamService;
     @Autowired
     GroupService groupService;
+    @Autowired
+    EmailService emailService;
     @GetMapping("/spam")
     public List<Spam> getAllSpam(){
         List<Spam> spamsList = spamService.getAllSpams();
@@ -33,7 +38,32 @@ public class SpamRestController {
     public Spam addNewSpam(@RequestBody Spam spam, @PathVariable("group_id") int id) {
         spam.setStatusCode('R');
         spam.setGroup(groupService.getGroup(id));
-        System.out.println(spam);
+        String theme = spam.getLetterTheme();
+        String content = spam.getLetterContent();
+
+        int count = 0, sent = 0, not_sent = 0;
+        for(User user : spam.getGroup().getUsers()){
+            EmailDetails emailDetails = new EmailDetails();
+            emailDetails.setRecipient(user.getEmail());
+            emailDetails.setSubject(theme);
+            emailDetails.setMsgBody(content);
+            if(emailService.sendSimpleMail(emailDetails)){
+                sent++;
+            } else {
+                not_sent++;
+            }
+        }
+
+        spam.setStatusCode('G');
+
+        if(not_sent > 0){
+            spam.setStatusCode('Y');
+        }
+
+        if(sent == 0){
+            spam.setStatusCode('R');
+        }
+
         spamService.saveSpam(spam);
         return spam;
     }
---
+++ src/main/java/dev/seratt/mailing_system_main/service/EmailServiceImpl.java
@@ -0,0 +1,35 @@
+package dev.seratt.mailing_system_main.service;
+
+import dev.seratt.mailing_system_main.entity.EmailDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService{
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}") private String sender;
+    public boolean sendSimpleMail(EmailDetails details) {
+        try {
+            // Creating a simple mail message
+            SimpleMailMessage mailMessage
+                    = new SimpleMailMessage();
+
+            // Setting up necessary details
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(details.getRecipient());
+            mailMessage.setText(details.getMsgBody());
+            mailMessage.setSubject(details.getSubject());
+
+            // Sending the mail
+            javaMailSender.send(mailMessage);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
