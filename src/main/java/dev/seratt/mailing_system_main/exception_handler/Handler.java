48c26cdbcc5cb4fa468fb18241396603a5bff516
Sayat <serat5915@gmail.com>
2023-06-20 14:36:59 +0600

    small fixes

--- src/main/java/dev/seratt/mailing_system_main/exception_handler/Handler.java
+++ src/main/java/dev/seratt/mailing_system_main/exception_handler/Handler.java
@@ -1,6 +1,7 @@
 package dev.seratt.mailing_system_main.exception_handler;
 
 import dev.seratt.mailing_system_main.exception.InvalidDataInfo;
+import dev.seratt.mailing_system_main.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class Handler {
     @ExceptionHandler
-    public ResponseEntity<InvalidDataInfo> handleException(NumberFormatException exception){
+    public ResponseEntity<InvalidDataInfo> handleException(UserNotFoundException exception){
         InvalidDataInfo info = new InvalidDataInfo("Bad request: " +
                 exception.getMessage());
         return new ResponseEntity<>(info, HttpStatus.BAD_REQUEST);
